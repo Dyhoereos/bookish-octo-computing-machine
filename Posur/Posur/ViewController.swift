74e2f89ac984e1efb1b78d8bde3a2c0f9b768fdf
Dyhoereos <dennis.chau@outlook.com>
2016-11-06 00:47:24 -0700

    Included landscape and portrait images. Change to appropriate type when device is rotated.

--- Posur/Posur/ViewController.swift
+++ Posur/Posur/ViewController.swift
@@ -13,24 +13,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.changeImage), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
         UIScreen.main.brightness = CGFloat(1.0)
-
-        let imgFiles = getListOfFileNames()
-        let randomIndex = Int(arc4random_uniform(UInt32(imgFiles.count)))
-        let imgFile = NSURL.fileURL(withPath: imgFiles[randomIndex]).deletingPathExtension()
-        if let imgPath = Bundle.main.path(forResource: imgFile.path, ofType: "png", inDirectory: "images") {
-            imgView.image = UIImage(contentsOfFile: imgPath)
-        }
+        changeImage()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    @objc private func changeImage(){
+        let imgFiles = getListOfFileNames()
+        let randomIndex = Int(arc4random_uniform(UInt32(imgFiles.count)))
+        let imgFile = NSURL.fileURL(withPath: imgFiles[randomIndex]).deletingPathExtension()
+        
+        if (isLandscape()) {
+            if let imgPath = Bundle.main.path(forResource: imgFile.path, ofType: "png", inDirectory: "h_images") {
+                imgView.image = UIImage(contentsOfFile: imgPath)
+            }
+        } else {
+            if let imgPath = Bundle.main.path(forResource: imgFile.path, ofType: "png", inDirectory: "p_images") {
+                imgView.image = UIImage(contentsOfFile: imgPath)
+            }
+        }
+    }
     
     private func getListOfFileNames() -> Array<String> {
         
-        let docsPath = Bundle.main.resourcePath! + "/images"
+        let docsPath = isLandscape() ?
+            Bundle.main.resourcePath! + "/h_images": Bundle.main.resourcePath! + "/p_images"
         let fileManager = FileManager.default
         var docsArray: [String] = []
         
@@ -39,10 +52,16 @@ class ViewController: UIViewController {
         } catch {
             print(error)
         }
-        
         return docsArray
     }
 
+    @IBAction func tapped(_ sender: AnyObject) {
+        changeImage()
+    }
+    
+    private func isLandscape() -> Bool{
+        return UIDevice.current.orientation.isLandscape
+    }
 
 }
 
